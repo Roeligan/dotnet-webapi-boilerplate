3633643df9dbf646093ba2dc4710f350a7bad559
Roel <roel.pauwels@gmail.com>
2023-07-18 11:53:32 +0200

    test

---
+++ src/Core/Application/Motocare/Articles/KTM/GetAdcountExportRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace FSH.WebApi.Application.Motocare.Articles.Ktm;
+
+public class GetAdcountExportRequest : IRequest
+{
+}
+
+public class GetAdcountExportRequestHandler : IRequest
+{
+    public async Task Handle(GetAdcountExportRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
